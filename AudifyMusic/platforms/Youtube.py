6981168fe961b155e47afa669b777ce65914341b
ᴅᴇᴠ..!! 🇮🇳 <190914293+karmaxexclusive@users.noreply.github.com>
2025-10-19 20:36:05 +0530

    Update Youtube.py

--- AudifyMusic/platforms/Youtube.py
+++ AudifyMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AloneX.utils.database import is_on_off
-from AloneX.utils.formatters import time_to_seconds
+from AudifyMusic.utils.database import is_on_off
+from AudifyMusic.utils.formatters import time_to_seconds
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
